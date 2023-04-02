37d6d66137c4528cf2a40b3d55fc3b4c90604c0d
viisualworks <dshege4@gmail.com>
2023-04-02 18:36:17 +0300

    add exhibition stats

--- page-curator-dashboard.php
+++ page-curator-dashboard.php
@@ -8,8 +8,33 @@ get_header(); ?>
     <h1 class="text-center"> <?php echo get_the_title(); ?> <h1>
 </div>
 
-<?php while(have_posts()){ the_post(); ?>
-    <?php the_content(); ?>
-<?php } ?>
+<div class="container">
+    <?php $current_user = wp_get_current_user();
+        echo "<h3 class='welcome-txt'>Welcome ".$current_user->display_name. "</h3>"
+    ?>
+
+    <div class=" exhibition-sxn">
+        <?php
+        $args = array(
+            'post_type' => 'product',
+            'posts_per_page' => -1,
+        );
+        $the_query = new WP_Query($args); ?>
+
+        <h4>Exhibition stats</h4>
+        <div class="row">
+            <?php if($the_query->have_posts()) : ?>
+            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+            <div class="col-md-4">
+                <?php the_post_thumbnail(); ?>
+                <h5><?php the_title(); ?></h5>
+            </div>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
+        </div>
+    </div>
+
+</div>
 
 <?php get_footer(); ?>
